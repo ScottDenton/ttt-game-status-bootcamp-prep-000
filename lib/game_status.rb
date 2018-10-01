533d26d443009f7f7143547299dc7e15f014fcee
scottyd_14 <scottyd_14@hotmail.com>
2018-10-01 23:22:50 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -27,7 +27,9 @@ WIN_COMBINATIONS.each {|win_combo|
 
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
       return win_combo
-       end
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return win_combo
+    end
   }
   return false
 end
