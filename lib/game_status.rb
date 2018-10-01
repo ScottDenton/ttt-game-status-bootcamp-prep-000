f4e69b86adb78ae820a55d8f13e363b835d90ffb
scottyd_14 <scottyd_14@hotmail.com>
2018-10-01 23:10:16 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -50,11 +50,9 @@ end
 
 def over?(board)
   if won?(board) || full?(board)|| draw?(board)
-    puts "The game is over"
-    return TRUE
+       return TRUE
   else
-    puts "The game is still in progress"
-    return FALSE
+       return FALSE
   end
   
 end
