3984189286623fa7d1580609e5f96afebcddbcfa
scottyd_14 <scottyd_14@hotmail.com>
2018-10-01 22:57:41 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -20,7 +20,7 @@ def won?(board)
     if board[win_combo[0]] == board[win_combo[1]] && board[win_combo[1]]==board[win_combo[2]] && position_taken?(board, win_combo[0])
       return win_combo
    end
-   if board = [" "]
+   if board = [" ", " ", " "," ", " ", " "," ", " ", " "]
   
   end
 end
