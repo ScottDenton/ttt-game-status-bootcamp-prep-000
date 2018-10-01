3afff0886cd4b440308aab19c75fc847cae43ef6
scottyd_14 <scottyd_14@hotmail.com>
2018-10-01 23:28:46 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -64,6 +64,19 @@ def over?(board)
   end
 end
 
+def win?(board)
+  winning_combo = won?(board)
+  if winning_combo == TRUE
+    if board[winning_combo[1]] =="O"
+  return "O"
+else
+  return "X"
+end
+end
+else 
+  return nil
+end
+
 
 
 
